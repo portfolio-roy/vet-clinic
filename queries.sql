1529100890dca3665f135c18bfddabb761863946
Utsargo Roy <royutsargo@gmail.com>
2023-02-28 23:25:46 +0600

    Add queries: - Animals belong to Melody Pond - List of all animals that are pokemon (their type is Pokemon) - List all owners and their animals, remember to include those that don't own any animal - Animals count per species - List all Digimon owned by Jennifer Orwell - List all animals owned by Dean Winchester that haven't tried to escape - Who owns the most animals?

--- queries.sql
+++ queries.sql
@@ -57,3 +57,48 @@ SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31'
 GROUP BY species;
+
+-- Animals belong to Melody Pond
+SELECT animals.name 
+FROM animals 
+JOIN owners ON animals.owner_id = owners.id 
+WHERE owners.full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon)
+SELECT animals.name 
+FROM animals 
+JOIN species ON animals.species_id = species.id 
+WHERE species.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal
+SELECT owners.full_name, animals.name 
+FROM owners 
+LEFT JOIN animals ON owners.id = animals.owner_id 
+ORDER BY owners.full_name;
+
+-- Animals count per species
+SELECT species.name, COUNT(*) as total 
+FROM animals 
+JOIN species ON animals.species_id = species.id 
+GROUP BY species.name;
+
+-- List all Digimon owned by Jennifer Orwell
+SELECT animals.name 
+FROM animals 
+JOIN species ON animals.species_id = species.id 
+JOIN owners ON animals.owner_id = owners.id 
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape
+SELECT animals.name 
+FROM animals 
+JOIN owners ON animals.owner_id = owners.id 
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+-- Who owns the most animals?
+SELECT owners.full_name, COUNT(*) as total 
+FROM animals 
+JOIN owners ON animals.owner_id = owners.id 
+GROUP BY owners.full_name 
+ORDER BY total DESC 
+LIMIT 1;
