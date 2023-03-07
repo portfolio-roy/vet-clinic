480c2cf18a79c1a435dbb158a756d88b389cf5b8
Thomas Obuya <thomasobuya95@gmail.com>
2023-03-07 15:50:41 +0300

    Create invoices and invoice_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,5 +21,20 @@ CREATE TABLE medical_history_treatments (
 CREATE INDEX fk_medical_history_treatments_medical_history_id_idx ON medical_history_treatments(medical_history_id);
 CREATE INDEX fk_medical_history_treatments_treatment_id_idx ON medical_history_treatments(treatment_id);
 
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INTEGER REFERENCES medical_histories(id),
+);
 
 
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INTEGER,
+  total_price DECIMAL,
+  invoice_id INTEGER REFERENCES invoices(id),
+  treatment_id INTEGER REFERENCES treatments(id),
+);
