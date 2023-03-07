e7fa32469ce7a6abd921a9bd0431e9f3f7bb43ed
Nelofar Zabi <mutau85@gmail.com>
2023-03-07 11:25:37 +0200

    Create medical_histories_table, Create_treatments_table, Create_join_table, Add_foreign_indexes

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,25 @@
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INTEGER NOT NULL,
+  status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE medical_history_treatments (
+  medical_history_id INTEGER NOT NULL REFERENCES medical_histories(id),
+  treatment_id INTEGER NOT NULL REFERENCES treatments(id),
+  PRIMARY KEY (medical_history_id, treatment_id)
+);
+
+-- Add foreign key indexes
+CREATE INDEX fk_medical_history_treatments_medical_history_id_idx ON medical_history_treatments(medical_history_id);
+CREATE INDEX fk_medical_history_treatments_treatment_id_idx ON medical_history_treatments(treatment_id);
+
+
+
